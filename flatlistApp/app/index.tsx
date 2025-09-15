4b19e2177d02ad259f84db2ac66d295c78350878
Joshua-HTML <joshuaverghese35@gmail.com>
2025-09-15 10:22:37 -0500

    work

--- flatlistApp/app/index.tsx
+++ flatlistApp/app/index.tsx
@@ -7,8 +7,31 @@ import {
 } from "react-native";
 import colors from "../styles/colors";
 import defaultStyles from "../styles/defaultStyles";
+import { useState } from "react";
+
 
 export default function Index() {
+
+  //Declare the data type for each item in the array
+  type dataType = {
+    id: string; //unique identifier 
+    title: string; //text displayed in flatlist
+  }
+
+  const DATA: dataType[] = [
+    {id:"1", title: "First Item"},
+    {id:"2", title: "Second Item"},
+    {id:"3", title: "Third Item"},
+    {id:"4", title: "Fourth Item"},
+  ];
+  // create a state car to keep track of selected id 
+  const [selectedId, setSelectedId] = useState<string>("1")
+
+  const itemSelected = (item: dataType) => {
+    console.log(item.title);
+    setSelectedId(item.id)
+  }
+
   return (
     <View style={defaultStyles.container}>
       <View style={defaultStyles.titleContainer}>
@@ -16,7 +39,21 @@ export default function Index() {
       </View>
       <View style={[defaultStyles.textContainer, { flex: 1 }]}>
         <View style={styles.flatlist}>
-          <Text>This is where our list will go</Text>
+          <FlatList
+              data ={DATA}
+              keyExtractor={(item: dataType) => item.id}
+              renderItem={({item}) => (
+                <TouchableOpacity onPress={() => itemSelected(item)}>
+                  <View style={[styles.flastListRow, 
+                  {
+                    backgroundColor:item.id ===selectedId ? colors.primary: colors.secondary
+
+                  }]}>
+                    <Text>{item.title}</Text>
+                  </View>
+                </TouchableOpacity>
+              )}
+          />
         </View>
       </View>
     </View>
@@ -27,6 +64,13 @@ const styles = StyleSheet.create({
   flatlist: {
     alignItems: "center",
   },
+  flastListRow:{
+    backgroundColor: 'lightgreen',
+    margin: 5,
+    padding:5,
+    width:150,
+  },
+
   titleContainer: {
     marginTop: 5,
     width: 300,
